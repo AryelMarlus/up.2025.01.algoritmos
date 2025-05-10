e0535568c583f3f95ba7f8e852a3b09bc14de759
aryel <aryelmarlus@gmail.com>
2025-05-10 16:13:14 -0300

    Adicionar arquivos de projeto.

---
+++ main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main() {
+	for (int i = 0; i <= 10; i++) {
+		printf("%d\n", i);
+	}
+
+	int contador = 0;
+
+	while (contador != 10) {
+		printf("%d\n", contador);
+		contador++;
+	}
+
+	if (contador == 10) {
+		printf("O valor do contador e %d\n", contador);
+	}
+
+
+
+}
